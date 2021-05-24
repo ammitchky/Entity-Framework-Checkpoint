66f5855a9ced726a2076ce5e40a6e017445f3495
Andrew Mitschke <ammitchky@gmail.com>
2021-05-24 13:38:41 -0500

    defined seed data for db / created first migration / built db using migration

--- App/Data/SchoolContext.cs
+++ App/Data/SchoolContext.cs
@@ -11,8 +11,25 @@ namespace App.Data
         protected override void OnConfiguring(DbContextOptionsBuilder options) 
         => options.UseSqlite("Data Source=school.db");
 
-        // protected override void OnModelCreating(ModelBuilder mb) {
-        //     mb.Entity<Student>().HasData()
-        // }
+        protected override void OnModelCreating(ModelBuilder mb) {
+            mb.Entity<Student>().HasData(
+                new Student { Id = 1, FirstName = "Samiel", LastName = "Jacobs", Age = 13, Classification = Student.SchoolYear.Freshman},
+                new Student { Id = 2, FirstName = "Sarah", LastName = "Vega", Age = 16, Classification = Student.SchoolYear.Junior},
+                new Student { Id = 3, FirstName = "Mark", LastName = "Plier", Age = 18, Classification = Student.SchoolYear.Senior},
+                new Student { Id = 4, FirstName = "Audrey", LastName = "White", Age = 14, Classification = Student.SchoolYear.Sophomore}
+            );
+
+            mb.Entity<Grade>().HasData(
+                new Grade { Id = 1, StudentId = 1, CourseName = "Geometry 102", CourseGrade = 87.7f },
+                new Grade { Id = 2, StudentId = 1, CourseName = "Art 201", CourseGrade = 98.7f },
+                new Grade { Id = 3, StudentId = 1, CourseName = "Chemistry 301", CourseGrade = 100.0f },
+                new Grade { Id = 4, StudentId = 1, CourseName = "English 102", CourseGrade = 82.6f },
+                new Grade { Id = 5, StudentId = 2, CourseName = "Chemistry 301", CourseGrade = 99.2f },
+                new Grade { Id = 6, StudentId = 2, CourseName = "Art 201", CourseGrade = 70.2f },
+                new Grade { Id = 7, StudentId = 3, CourseName = "English 102", CourseGrade = 93.5f },
+                new Grade { Id = 8, StudentId = 3, CourseName = "Art 301", CourseGrade = 100.0f },
+                new Grade { Id = 9, StudentId = 4, CourseName = "Art 301", CourseGrade = 65.0f }
+            );
+        }
     }
 }
---
+++ App/Migrations/20210524183504_Initial db setup.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace App.Migrations
+{
+    public partial class Initialdbsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: true),
+                    LastName = table.Column<string>(type: "TEXT", nullable: true),
+                    Age = table.Column<int>(type: "INTEGER", nullable: false),
+                    Classification = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Grades",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StudentId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CourseName = table.Column<string>(type: "TEXT", nullable: true),
+                    CourseGrade = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grades", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Grades_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Age", "Classification", "FirstName", "LastName" },
+                values: new object[] { 1, 13, 0, "Samiel", "Jacobs" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Age", "Classification", "FirstName", "LastName" },
+                values: new object[] { 2, 16, 2, "Sarah", "Vega" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Age", "Classification", "FirstName", "LastName" },
+                values: new object[] { 3, 18, 3, "Mark", "Plier" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Age", "Classification", "FirstName", "LastName" },
+                values: new object[] { 4, 14, 1, "Audrey", "White" });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseGrade", "CourseName", "StudentId" },
+                values: new object[] { 1, 87.7f, "Geometry 102", 1 });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseGrade", "CourseName", "StudentId" },
+                values: new object[] { 2, 98.7f, "Art 201", 1 });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseGrade", "CourseName", "StudentId" },
+                values: new object[] { 3, 100f, "Chemistry 301", 1 });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseGrade", "CourseName", "StudentId" },
+                values: new object[] { 4, 82.6f, "English 102", 1 });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseGrade", "CourseName", "StudentId" },
+                values: new object[] { 5, 99.2f, "Chemistry 301", 2 });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseGrade", "CourseName", "StudentId" },
+                values: new object[] { 6, 70.2f, "Art 201", 2 });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseGrade", "CourseName", "StudentId" },
+                values: new object[] { 7, 93.5f, "English 102", 3 });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseGrade", "CourseName", "StudentId" },
+                values: new object[] { 8, 100f, "Art 301", 3 });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseGrade", "CourseName", "StudentId" },
+                values: new object[] { 9, 65f, "Art 301", 4 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Grades_StudentId",
+                table: "Grades",
+                column: "StudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Grades");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
